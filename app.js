36294c53911b8c1c58e581efeb94f6329ec54bbe
hsiaomb <hsiaomb@usc.edu>
2015-11-20 15:45:13 +0000

    Folder structure created

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var ejs = require('ejs');
+var ejsLayouts = require('express-ejs-layouts');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parsers');
+var session = require('express-session');
+var methodOverride = require('method-override');
+
+//Setting up the database
+mongoose.connect('mongodb://localhost/recipeeps');
+
+//Setting up middleawre
+app.use(morgan('dev')); 
+app.use(cookieParser());
+app.use(bodyParser()); 
+
+//Setting up the views
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.set("views","./views");
+
+//Set up our public folder
+app.use(express.static(__dirname + '/public'));
+
+//Setting up routes
+var router = require(__dirname + '/config/routes');
+app.use(router);
+
+app.get("/", function (req, res){
+	res.redirect("/sports");
+});
+
+app.listen(3000);
+console.log("Express is listening on localhost:3000");
